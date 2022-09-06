6525a92a986009731c6103a751e356faeb3a7358
Patrick Kalita <pkalita@lbl.gov>
2022-09-06 13:57:57 -0700

    Fix lint issue

--- src/prefixmaps/__init__.py
+++ src/prefixmaps/__init__.py
@@ -1,6 +1,6 @@
 try:
     from importlib.metadata import version
-except ImportError: # for Python<3.8
+except ImportError:  # for Python<3.8
     from importlib_metadata import version
 
 __version__ = version(__name__)
