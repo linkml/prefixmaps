c73de853e6918f30598cd76668d477bc88880f58
Patrick Kalita <pkalita@lbl.gov>
2022-09-06 13:54:42 -0700

    Use importlib_metadata compatibility lib for Python<3.8

--- src/prefixmaps/__init__.py
+++ src/prefixmaps/__init__.py
@@ -1,3 +1,6 @@
-from importlib import metadata
+try:
+    from importlib.metadata import version
+except ImportError: # for Python<3.8
+    from importlib_metadata import version
 
-__version__ = metadata.version(__name__)
+__version__ = version(__name__)
