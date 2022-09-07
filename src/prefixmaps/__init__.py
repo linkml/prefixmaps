8de015379ad0688fd4aa655a5226dd08f48e5b11
Harshad Hegde <hegdehb@gmail.com>
2022-09-07 17:05:15 -0500

    linted

--- src/prefixmaps/__init__.py
+++ src/prefixmaps/__init__.py
@@ -1,11 +1,12 @@
 from importlib import metadata
+
 from .io.parser import load_context
 
 try:
     from importlib.metadata import version
 except ImportError:  # for Python<3.8
     from importlib_metadata import version
-    
+
 __all__ = [
     "load_context",
 ]
